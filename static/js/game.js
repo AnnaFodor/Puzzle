3f0b91695646cbe976f9762f14daf5ca4c2950d3
AlinaGoor <gooralina@gmail.com>
2018-12-12 12:46:01 +0100

    Pics in generated random order

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,59 @@
+// let rowFirst
+//
+// let piece1 = `<div id="piece1"></div>`,
+//     piece2 = `<div id="piece2"></div>`,
+//     piece3 = `<div id="piece3"></div>`,
+//     piece4 = `<div id="piece4"></div>`,
+//     piece5 = `<div id="piece5"></div>`,
+//     piece6 = `<div id="piece6"></div>`,
+//     piece7 = `<div id="piece7"></div>`,
+//     piece8 = `<div id="piece8"></div>`,
+//     piece9 = `<div id="piece9"></div>`;
+//
+//
+// let listOfPieces = [piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9]
+//
+//
+// for (let piece of listOfPieces){
+//     rowFirst.innerHTML += piece;
+// }
 
+function shuffle(arr) {
+    var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
+
+
+
+function gen_pieces(number) {
+    let rowFirst = document.querySelector(".puzzle");
+    let tmp = []
+    for (let i = 0; i < number; ++i) {
+        let div = document.createElement("div");
+        div.setAttribute("id",`piece${i+1}`);
+        tmp.push(div);
+    }
+    tmp = shuffle(tmp);
+    for(let element of tmp){
+        rowFirst.appendChild(element);
+    }
+}
+
+
+
+function clickHandler(event) {
+    rowFirst = document.querySelector(".puzzle")
+    rowFirst.innerHTML(piece3,piece3,piece1)
+}
+
+
+gen_pieces(9);
